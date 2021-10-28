c6a86ada500a5805f788bd29d19b7974f3763428
TheWa1rus <cab350@pitt.edu>
2021-10-28 11:45:58 -0400

    UTOPIA-33 updated main.py to format first and last name files as csv. fixed bugs in UserGenerator, added json parsing for user object. Now only need to add POST request to API.

--- UserGenerator.py
+++ UserGenerator.py
@@ -1,5 +1,5 @@
 import random
-from password_generator import PasswordGenerator
+from easy_password_generator import PassGen
 import json
 
 # seed random for deterministic results
@@ -9,12 +9,12 @@ random.seed(100)
 first_names = []
 with open("firstnames.txt", "r") as fnames:
     for line in fnames.readlines():
-        first_names = first_names.append(line.split(","))
+        first_names.extend(line.split(","))
 
 last_names = []
 with open("lastnames.txt", "r") as lnames:
     for line in lnames.readlines():
-        last_names = last_names.append(line.split(","))
+        last_names.extend(line.split(","))
 
 
 def f_name():
@@ -27,12 +27,25 @@ def l_name():
 
 # get random phone number
 # parameter n allows for country codes to be added, though default will be 10 digits
-def rand_phone(n = 10):
+def rand_phone(n =10):
     range_min = 10**(n-1)
     range_max = (10**n) -1
-    return random.sample(range(range_min, range_max))
+    return random.randint(range_min, range_max)
 
 
+def get_email(uname):
+    domain = random.randint(1, 4)
+    if domain == 1:
+        domain = "@gmail.com"
+    elif domain == 2:
+        domain = "@yahoo.com"
+    elif domain == 3:
+        domain = "@protonmail.com"
+    elif domain == 4:
+        domain = "@mail.com"
+    return uname + domain
+
+# included for possible future use of generating passengers
 def rand_dob():
     year = random.randint(0, 120) + 1901
     month = random.randint(1, 12)
@@ -51,12 +64,41 @@ def rand_dob():
     return [year, month, day]
 
 
+def get_role():
+    r_id = random.randint(1,4)
+    r_name = ""
+    if r_id == 1:
+        r_name = "ROLE_USER"
+    elif r_id == 2:
+        r_name = "ROLE_AGENT"
+    elif r_id == 3:
+        r_name = "ROLE_GUEST"
+    elif r_id == 4:
+        r_name = "ROLE_ADMIN"
+    return [r_id, r_name]
+
+
 def gen_pass():
-    pwo = PasswordGenerator()
+    pwo = PassGen()
     # defaults include 1 digit, 1 uppercase letter, 1 lowercase letter, and 1 symbol
-    # total length of 6-16 degits by default
+    # total length of 6-16 digits by default
     return pwo.generate()
 
 num_users = int(input("Enter the number of users to be generated: "))
 user = ""
 for num in range(num_users):
+    role = get_role()
+    fname = f_name()
+    lname = l_name()
+    username = fname[0:4] + "." + lname
+    email = get_email(username)
+    password = gen_pass()
+    phone = rand_phone()
+
+    user = '{"id":0, "role_id":' + str(role[0]) + ', "given_name":"' + fname + '", "family_name":"' + lname +\
+           '", "username":"' + username + '", "email":"' + email + '", "password":"' + password +\
+           '", "phone":"' + str(phone) + '", "roles":"' + role[1] + '"}'
+    json_user = json.loads(user)
+    print(json_user)
+
+    # create POST request
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import names
 with open("lastnames.txt", "w") as lnames:
     for i in range(1000):
         lnames.write(names.get_last_name())
+        lnames.write(",")
 
 with open("firstnames.txt", "w") as fnames:
     for i in range(500):
         fnames.write(names.get_first_name(gender="female"))
+        fnames.write(",")
     for i in range(500):
         fnames.write(names.get_first_name(gender="male"))
+        fnames.write(",")
 
